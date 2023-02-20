91d53781b129099a83d3af1e3d379c0282d07adf
Ann Sarviro <annka_nuha@ai.ua>
2023-02-20 23:46:10 +0200

    add adminAuthenticate middleware

--- middleware/index.js
+++ middleware/index.js
@@ -2,10 +2,11 @@ const validation = require("./validation");
 const upload = require("./upload");
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
-
+const adminAuthenticate = require("./adminAuthenticate");
 module.exports = {
   validation,
   authenticate,
   isValidId,
+  adminAuthenticate,
   upload,
 };
--- routes/partners.js
+++ routes/partners.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { validation, authenticate } = require("../middleware/");
+const { validation, adminAuthenticate } = require("../middleware/");
 
 const { asyncWrapper } = require("../helpers");
 
@@ -11,9 +11,9 @@ const { partnersCTRL: ctrl } = require("../controllers");
 
 router.get("/", asyncWrapper(ctrl.getAllPartnersCTRL));
 router.get("/:id", asyncWrapper(ctrl.getPartnerByIdCTRL));
-router.post("/", authenticate, validation(partnersSchema), asyncWrapper(ctrl.addPartnersCTRL));
-router.patch("/:id/:properties", authenticate, asyncWrapper(ctrl.updatePartnerCTRL));
-router.patch("/logo/:id", authenticate, asyncWrapper(ctrl.updateLogoPartnerCTRL));
-router.delete("/:id", authenticate, asyncWrapper(ctrl.deletePartnerCTRL));
+router.post("/", adminAuthenticate, validation(partnersSchema), asyncWrapper(ctrl.addPartnersCTRL));
+router.patch("/:id/:properties", adminAuthenticate, asyncWrapper(ctrl.updatePartnerCTRL));
+router.patch("/logo/:id", adminAuthenticate, asyncWrapper(ctrl.updateLogoPartnerCTRL));
+router.delete("/:id", adminAuthenticate, asyncWrapper(ctrl.deletePartnerCTRL));
 
 module.exports = router;
